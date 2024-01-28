b37b2a383b973b818aad75d04c7de455d3f3cfc1
Assunta Della Porta <a.dellaporta28@studenti.unisa.it>
2024-01-28 21:46:28 +0100

    argparser and backgrounf subtraction

---
+++ TOOLS/argparser.py
@@ -0,0 +1,27 @@
+import argparse
+
+class parser(): 
+
+    def __init__(self): 
+        parser = argparse.ArgumentParser(description='AV parser')
+        parser.add_argument('--video', help='.mp4 video to process')
+        parser.add_argument('--configuration', help='.txt rois configuration')
+        parser.add_argument('--results', help='.txt file on wich write the results')
+        self.parser = parser 
+
+    def parse(self): 
+        args = self.parser.parse_args()
+        return args
+    
+
+# --------------- USAGE ----------------------
+
+# def main(): 
+#     pars = parser()
+#     args = pars.parse()
+#     print('video = '+args.video)
+#     print('configuration = '+args.configuration)
+#     print('results = '+args.results)
+
+# if __name__ == '__main__': 
+#     main()s
---
+++ TOOLS/bgSubtractor.py
@@ -0,0 +1,89 @@
+import torch
+import torchvision.transforms as T
+from torchvision import models
+from PIL import Image
+# I also needed to install: pip install --upgrade typing-extensions
+
+
+class bgRemover(): 
+    
+    def __init__(self): 
+        # Loads the model 
+        model = models.segmentation.deeplabv3_resnet101(pretrained=True)
+        model.eval()
+        self.model = model
+        # simple transform that makes an image to tensor 
+        self.simple_transform = T.Compose([T.ToTensor()])
+        # transform that normalize the tensor so it's ready for the model 
+        # self.normalization_transform = T.Compose([T.ToPILImage(), T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        self.normalization_transform = T.Compose([T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        
+    
+    def _img2tensor(self, img_path, normalization=True):
+        img = Image.open(img_path).convert("RGB")
+        if normalization == True: 
+            tensor = self.simple_transform(img)
+            tensor = self.normalization_transform(tensor).unsqueeze(0)
+        else: 
+            tensor = self.simple_transform(img).unsqueeze(0)
+        return tensor 
+    
+    def _tensor2img(self, tensor, output_img_path=None):
+        '''Converts the input tensor to an image. 
+        It returns the image and also saves it into output_img_path if required'''
+        img = T.ToPILImage()(tensor.squeeze(0))
+        if output_img_path is not None: 
+            img.save(output_img_path)
+        return img
+    
+    def _getMask(self, tensor):
+        with torch.no_grad():
+            output = self.model(tensor)['out'][0]
+        output_predictions = output.argmax(0)
+        # Create a mask where background pixels are set to 0
+        mask = (output_predictions == 0).float()
+        mask = 1 - mask
+        return mask 
+    
+    def _bgRemove(self, tensor, mask): 
+        '''!!! please note !!! tensor is the NOT NORMALIZED tensor from the image'''
+        res = tensor * mask
+        return res
+    
+    def bgr_img(self, img_path, out_path):
+        '''removes the bg from an image and returns an image'''
+        normalized_tensor = self._img2tensor(img_path, normalization=True)
+        mask = self._getMask(normalized_tensor)
+        tensor = self._img2tensor(img_path, normalization=False)
+        out = self._bgRemove(tensor, mask)
+        out_img = self._tensor2img(out, out_path)
+        return out_img
+    
+    def bgr_tensor(self, in_tensor):
+        '''removes the bg from an image and returns an image'''
+        normalized_tensor = self.normalization_transform(in_tensor.squeeze(0)).unsqueeze(0)
+        mask = self._getMask(normalized_tensor)
+        out = self._bgRemove(in_tensor, mask)
+        return out
+
+    
+
+# ----------- USAGE ---------------
+
+# def main(): 
+#     img_path = 'test.png'
+#     out_path = 'output.jpg'
+#     out_tensor_path = 'out_tensor.jpg'
+
+#     bgr = bgRemover()
+#     bgr.bgr_img(img_path, out_path)
+#     tensor = bgr._img2tensor(img_path, normalization=False)
+#     out = bgr.bgr_tensor(tensor)
+#     bgr._tensor2img(out, out_tensor_path)
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+
